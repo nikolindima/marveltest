b4808d702d8fb3f8ed92489a4474eab59c5210e2
nikolindima <nikolindima@gmail.com>
2020-05-27 11:40:16 +0300

    Initial commit

---
+++ marveltest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marveltest
+//
+//  Created by Dmitriy Nikolin on 27/05/2020.
+//  Copyright © 2020 dnikolin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
